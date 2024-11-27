718f2c387a5e319d4c35367c00e24fad41891552
BxsedSosa <donovanxabad@protonmail.com>
2024-11-26 17:59:21 -0800

    completed q2

--- intro-book/intro-to-programming/js/age.js
+++ intro-book/intro-to-programming/js/age.js
@@ -1,5 +1,6 @@
 const AGE = 20;
 
-for (let i = 10; i <= 40; i + 10) {
+console.log(`You are ${AGE} years old.`);
+for (let i = 10; i <= 40; i += 10) {
   console.log(`In ${i} years, you will be ${i + AGE} years old.`);
 }
