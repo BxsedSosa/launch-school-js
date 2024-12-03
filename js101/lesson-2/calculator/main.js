2c75ec85962140a07c94f26aa609bca9cd8784dc
BxsedSosa <donovanxabad@gmail.com>
2024-12-03 10:32:02 -0800

    Added readline sync

--- js101/lesson-2/calculator/main.js
+++ js101/lesson-2/calculator/main.js
@@ -3,3 +3,17 @@
  * - Ask the user for the type of operation to perform: add, subtract, multiply or divide
  * - Perform the calculation and display the result
  */
+
+function main() {}
+
+function askUserNumbers() {
+  //pass
+}
+
+function askUserOperation() {
+  //pass
+}
+
+function calculateNumbers(numbers, operation) {
+  //pass
+}
