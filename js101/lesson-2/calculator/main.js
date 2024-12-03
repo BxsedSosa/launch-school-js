adf7a97f946987cc1ea8f3e202bd0422a73addb7
BxsedSosa <donovanxabad@gmail.com>
2024-12-03 10:27:42 -0800

    Created Calculator folder and main file

---
+++ js101/lesson-2/calculator/main.js
@@ -0,0 +1,5 @@
+/* To do List:
+ * - Ask the user for two numbers
+ * - Ask the user for the type of operation to perform: add, subtract, multiply or divide
+ * - Perform the calculation and display the result
+ */
