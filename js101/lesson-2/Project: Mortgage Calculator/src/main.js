5e0a787288a343e2baf532207ef18e5df439b2b0
BxsedSosa <donovanxabad@gmail.com>
2024-12-06 08:24:18 -0800

    Making sure everything is working

--- js101/lesson-2/Project: Mortgage Calculator/src/main.js
+++ js101/lesson-2/Project: Mortgage Calculator/src/main.js
@@ -1,3 +1,7 @@
 const RL_SYNC = require("readline-sync");
 const NOT_VALID = require("./validation");
 const MSG = require("../config/text.json");
+
+let x = RL_SYNC.question("Please enter:\n");
+
+console.log(x);
