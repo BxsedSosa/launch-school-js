e0c1a22253f2098a704eca301638cac032a7c6f9
BxsedSosa <donovanxabad@protonmail.com>
2024-12-28 17:59:22 -0800

    created user selection to change the grid

--- js110/lesson-3/tic-tac-toe/src/main.js
+++ js110/lesson-3/tic-tac-toe/src/main.js
@@ -5,6 +5,9 @@ main();
 function main() {
   let grid = createGrid();
   displayGameGrid(grid);
+  let userInput = getPlayerSelection();
+  grid = changeGrid(grid, userInput, "user");
+  displayGameGrid(grid);
 }
 
 function displayGameGrid(grid) {
@@ -31,8 +34,20 @@ function createGrid() {
     });
 }
 
+function changeGrid(grid, playerInput, whichPlayer) {
+  if (whichPlayer === "user") {
+    grid[playerInput[0]][playerInput[1]] = "X";
+    return grid;
+  }
+
+  grid[(playerInput[0], playerInput[1])] = "O";
+  return grid;
+}
+
 function getPlayerSelection() {
   // Get the players grid selection
+  let userInput = rl_sync.question("Enter something: \n");
+  return getMapSelection(userInput);
 }
 
 function getCpuSelection() {
@@ -41,6 +56,22 @@ function getCpuSelection() {
 
 function getPlayerRetry() {
   // Check if player wants to replay
+}
+
+function getMapSelection(playerInput) {
+  let gridMap = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+  };
+
+  return gridMap[playerInput];
 }
 
 function checkThreeInRow() {
