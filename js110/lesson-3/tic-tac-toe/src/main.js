57e7ca6e2e91cdcc3a9650ddcd925e8d8481dace
BxsedSosa <donovanxabad@protonmail.com>
2024-12-28 16:43:58 -0800

    setting up functions

--- js110/lesson-3/tic-tac-toe/src/main.js
+++ js110/lesson-3/tic-tac-toe/src/main.js
@@ -5,3 +5,23 @@ function main() {}
 function createGrid() {
   // Create the 3x3 grid for the play area and return it
 }
+
+function getPlayerSelection() {
+  // Get the players grid selection
+}
+
+function getCpuSelection() {
+  // Get CPU grid selection
+}
+
+function getPlayerRetry() {
+  // Check if player wants to replay
+}
+
+function checkThreeInRow() {
+  // Verify for winner after each players turn
+}
+
+function checkThreeWins() {
+  // Verify for player with 3 wins
+}
