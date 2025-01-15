2618dc56f1665361d49d3af5b0b6d97ffa0d1b2f
BxsedSosa <donovanxabad@gmail.com>
2025-01-15 09:07:29 -0800

    updated main

--- js110/lesson-3/blackjack/src/main.js
+++ js110/lesson-3/blackjack/src/main.js
@@ -2,5 +2,9 @@ import rlSync from "readline-sync";
 import MSG from "../config/text.json" assert { type: "json" };
 
 function main() {
-  //pass
+  // pass
+}
+
+function createDeck() {
+  // pass
 }
