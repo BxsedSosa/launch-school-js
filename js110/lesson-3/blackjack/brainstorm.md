f5e2a45e42b2fc7eb1413d330f1f29182944541f
BxsedSosa <donovanxabad@gmail.com>
2025-01-15 12:34:09 -0800

    created card display

--- js110/lesson-3/blackjack/brainstorm.md
+++ js110/lesson-3/blackjack/brainstorm.md
@@ -7,6 +7,15 @@
   - [x] 4 suits (Club, Spade, Diamond, Heart)
   - [x] 9 Number cards, 4 Face Cards (2 - 10 numbers) (Jack, Queen, King, Ace)
 
+- [ ] Card distribution
+
+  - [ ] 2 Cards to player and dealer
+    - [ ] pass cards in this order:
+      - Player
+      - Dealer
+      - Player
+      - Hidden Dealer Card from player
+
 - [ ] Need a dealer hand
 
   - [ ] Show 1 card and other card is hidden
--- js110/lesson-3/blackjack/src/main.js
+++ js110/lesson-3/blackjack/src/main.js
@@ -1,8 +1,22 @@
 import rlSync from "readline-sync";
 // import MSG from "../config/text.json" assert { type: "json" };
 
+main();
+
 function main() {
-  let deck = createDeck();
+  let deck = shuffleDeck();
+  let playerChips = getChips();
+  let playerHand = [];
+  let dealerHand = [];
+
+  startHand(deck, playerHand, dealerHand);
+}
+
+function startHand(deck, player, dealer) {
+  for (let i = 0; i < 2; i++) {
+    giveCard(deck, player);
+    giveCard(deck, dealer);
+  }
 }
 
 function createDeck() {
@@ -20,12 +34,72 @@ function createDeck() {
     .flat();
 }
 
-function shuffleDeck(deck) {
-  let newDeck = [...deck];
+function giveCard(deck, hand) {
+  hand.push(deck.shift());
+}
 
-  return newDeck.sort(() => Math.random() - 0.5);
+function shuffleDeck() {
+  return createDeck().sort(() => Math.random() - 0.5);
 }
 
 function getChips() {
   return 100;
 }
+
+function resetDeck(deck) {
+  return shuffleDeck();
+}
+
+function getSuits(hand) {
+  return hand.map((card) => {
+    return card[0];
+  });
+}
+
+function getValues(hand) {
+  return hand.map((card) => {
+    return card[1];
+  });
+}
+
+function displayCards(hand, player = false) {
+  let suits = getSuits(hand);
+  let values = getValues(hand);
+
+  if (player) {
+    console.log("Player Hand: ");
+  } else {
+    console.log("Dealer Hand: ");
+  }
+
+  let display = Array(suits.length)
+    .fill()
+    .map((_, idx) => {
+      return createTextCard(suits[idx], values[idx]);
+    })
+    .map((string) => string.split("\n"));
+
+  let organized = display[0].map((_, cardIdx) => {
+    return display.map((_, displayIdx, arr) => {
+      return arr[displayIdx][cardIdx];
+    });
+  });
+
+  for (let line of organized) {
+    console.log(line.join("  "));
+  }
+}
+
+function createTextCard(suit, value) {
+  if (value === 10) {
+    return ` ------ \n|${suit}     |\n|  ${value}  |\n|      |\n ------ `;
+  }
+  return ` ------ \n|${suit}     |\n|   ${value}  |\n|      |\n ------ `;
+}
+
+displayCards([
+  ["D", 10],
+  ["C", 5],
+  ["H", 5],
+  ["S", 3],
+]);
