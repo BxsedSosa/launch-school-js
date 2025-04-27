460f54c93d8479b26d16eac596073fbb0ec84aac
BxsedSosa <donovanxabad@protonmail.com>
2025-04-27 12:33:58 -0700

    completed p8

---
+++ js110/interview/P8/main.js
@@ -0,0 +1,32 @@
+let longestVowelSubstring = (string) => {
+  const RE = /[aeiou]/;
+  let chars = string.split("");
+
+  let count = chars.reduce((highestCount, currEle, idx) => {
+    let currentHighest = 0;
+
+    if (RE.test(currEle)) {
+      currentHighest += 1;
+      for (let char of chars.slice(idx + 1)) {
+        if (RE.test(char)) {
+          currentHighest += 1;
+        } else {
+          break;
+        }
+      }
+    }
+    if (currentHighest > highestCount) return currentHighest;
+
+    return highestCount;
+  }, 0);
+  return count;
+};
+
+const p = console.log;
+p(longestVowelSubstring("cwm") === 0);
+p(longestVowelSubstring("many") === 1);
+p(longestVowelSubstring("launchschoolstudents") === 2);
+p(longestVowelSubstring("eau") === 3);
+p(longestVowelSubstring("beauteous") === 3);
+p(longestVowelSubstring("sequoia") === 4);
+p(longestVowelSubstring("miaoued") === 5);
