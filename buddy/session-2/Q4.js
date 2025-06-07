9062bb2b04b8b8849e2ee4e788697a089bb70613
BxsedSosa <donovanxabad@gmail.com>
2025-06-07 09:25:34 -0700

    4 questions completed

---
+++ buddy/session-2/Q4.js
@@ -0,0 +1,20 @@
+/*
+ * 4. Identify all the variables
+ */
+
+let a = {
+  aa: "1",
+  bb: "2",
+  cc: "3",
+};
+
+let b,
+  c = "hello";
+
+d = "hi";
+
+const e = ["foo", "bar"];
+
+function qux() {
+  return "bax";
+}
