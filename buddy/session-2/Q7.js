3c0c09e2fcc4c7342f78e7b4a52ec40701ed2987
BxsedSosa <donovanxabad@gmail.com>
2025-06-07 09:51:24 -0700

    7th question completed

---
+++ buddy/session-2/Q7.js
@@ -0,0 +1,11 @@
+/*
+ * 7. What is happening and how can you fix the output?
+ */
+
+let newJeans = "Super Shy";
+
+for (let i = 0; i < newJeans.length; i++) {
+  newJeans[i] = newJeans[i].toUpperCase();
+}
+
+console.log(newJeans);
