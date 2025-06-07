ad355955dfe9e3570ddd7e8e9d4a4566978bff4a
BxsedSosa <donovanxabad@gmail.com>
2025-06-07 08:57:53 -0700

    1 finished

---
+++ buddy/session-2/Q1.js
@@ -0,0 +1,11 @@
+/*
+ * 1. Identify the Primitives and Objects
+ */
+
+let a = [1, 2, 3, []];
+
+let b = a.slice().map((ele) => ele * 2);
+
+let c = "Launch School";
+
+let d = c.split("").map((ele) => ele.toUpperCase());
